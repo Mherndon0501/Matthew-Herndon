e6553457a4ce18623cf23d595aca61fda60bd798
Mherndon0501 <100631460+Mherndon0501@users.noreply.github.com>
2023-08-31 13:58:58 +0000

    This is my final work! Super Simple

---
+++ firstprogram!/shortstory.java
@@ -0,0 +1,19 @@
+class shortstory
+{
+ public static void main(String args[])
+ {
+       System.out.println("Long ago in a land far away");
+       System.out.println("");
+       System.out.println("Lived a boy and his mother in the dark woods");
+        System.out.println("There was a monster in the woods");
+        System.out.println("But the mother captured it and put it in the basement");
+        System.out.println("The monster would hiss and growl and kept the boy awake");
+        System.out.println("So to put the child asleep, she sang him a lulaby");
+        System.out.println("The monster stopped to listen, and learned the tune");
+        System.out.println("The next day the mother went out to forge for food");
+        System.out.println("So the monster sang to the child");
+        System.out.println("The child was then lured to the basement door");
+        System.out.println("");
+        System.out.println("Then he opened it");
+ }
+}
