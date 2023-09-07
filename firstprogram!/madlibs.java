cd0f5169d153ddeb30f10648a4456a20151516f7
Mherndon0501 <100631460+Mherndon0501@users.noreply.github.com>
2023-09-07 13:57:08 +0000

    made madlibs

---
+++ firstprogram!/madlibs.java
@@ -0,0 +1,22 @@
+class madlibs
+{
+ public static void main(String args[])
+ {
+    String place1 = "Wild West";
+    String person1 = "Gunslinger";
+    String animal = "Horse";
+    String place2 = "Town";
+    String person2 = "Bandit";
+    String verb = "Rob";
+    String place3 = "Bank";
+    String person3 = "Sheriff";
+    String noun = "Gun";
+    String person4 = "Robber";
+
+    System.out.println("Years ago, in the " + place1);
+    System.out.println("There was a lone " + person1 + " with his trusty " + animal);
+    System.out.println("As he was riding into " + place2 +", there was a " + person2 + ", who tried to " + verb + " the " + place3);
+    System.out.println("But with the help of the " + person3 + ", and using his " + noun + ", they were able to elemitnate the " + person4);
+    System.out.println("The End");
+ }
+}
