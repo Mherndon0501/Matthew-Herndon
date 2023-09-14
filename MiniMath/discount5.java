d32a61430c5d3edc3f0d46c2b5f4637ea7e78711
Mherndon0501 <100631460+Mherndon0501@users.noreply.github.com>
2023-09-14 14:09:10 +0000

    this is 2.0 for the mini math problems

--- MiniMath/discount5.java
+++ MiniMath/discount5.java
@@ -3,7 +3,7 @@ class discount5
  public static void main(String args[])
  {
      double price = 203.0;
-     int savings = (int)(203.0 / 100.0 * 20.0);
+     int savings = (int)(price / 100.0 * 20.0);
      System.out.println(price - savings  + " is your total. You saved " + savings + " Dollars!");
  }
 }
