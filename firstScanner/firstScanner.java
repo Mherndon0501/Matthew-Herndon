e7f055d922b5f21f44c9c5e68890b17524d94e3a
Mherndon0501 <100631460+Mherndon0501@users.noreply.github.com>
2023-09-27 14:44:54 +0000

    This is the final project!

--- firstScanner/firstScanner.java
+++ firstScanner/firstScanner.java
@@ -10,12 +10,24 @@ class firstScanner
         System.out.println("Enter here:");
         int money = scanner.nextInt();
 
-        System.out.print("You have " + money + " dollars left");
+        System.out.println("You have " + money + " dollars right now");
 
-        System.out.println("The Government (me) infroced a higher tax policy.");
+        System.out.println("");
+        System.out.println("");
+
+        System.out.println("The Government (me) infroced a higher tax policy. I'm now taking 50% of your money");
         money /= 2;
         System.out.println("You now have " + money + " dollars left");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Would you look at that! You found a dollar on the floor!");
 
-
+        money ++;
+        System.out.println("You now have " + money + " dollars left");
+        
+        System.out.println("So, turns out that Playstation got hacked. A bunch of hackers stole that money, and now Playstation is giving you your money back.");
+        System.out.println("How much money did they give you? Enter here:");
+        int extraCash = scanner.nextInt();
+        System.out.println(extraCash + money + " Is how much money you have right now");
     }
 }
