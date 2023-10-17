97b1bae91f83581d87c7f12d2ea2e8ac50c2c569
Mherndon0501 <100631460+Mherndon0501@users.noreply.github.com>
2023-10-17 14:03:14 +0000

    im fiminsh with the if statmemntss35

--- ifStatements/whatIsThatLetter.java
+++ ifStatements/whatIsThatLetter.java
@@ -6,17 +6,17 @@ class letterTime
     {
         Scanner scanner = new Scanner(System.in);
         System.out.println("What is your letter?");
-        System.out.println("(Only enter a number, no signs, no words, ONLY NUMBERS)");
-        System.out.println("Enter here:");
-        int letter = scanner.nextInt();
+        System.out.println("(Only enter a single letter, no signs, no words, ONLY LETTER)");
+        System.out.println("Enter CAPITAL letter here:");
+        char letter = scanner.next().charAt(0);
 
-        if (charletter == 65)
+        if ((letter == 65) || (letter == 69) || (letter == 73) || (letter == 79) || (letter == 85))
         {
-            System.out.println("Your number is even! :)"); 
+            System.out.println("Your letter is a vowel! :)"); 
         }
         else
         {
-            System.out.println("Your number is odd :(");
+            System.out.println("Your letter is not a vowel :(");
         }
 
     }
