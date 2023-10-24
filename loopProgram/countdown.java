7a3a7283ae888c94ea40270d0bad5e6c6aba9a54
Mherndon0501 <100631460+Mherndon0501@users.noreply.github.com>
2023-10-24 15:45:06 +0000

    2 are completed in the ifstatement project

---
+++ loopProgram/countdown.java
@@ -0,0 +1,14 @@
+        class countdown
+{
+    public static void main(String args[])
+    {
+        int num = 100;
+
+        while (num >= 0)
+        {
+            System.out.println(num);
+            num--;
+            num--;
+        }
+    }
+}
---
+++ loopProgram/factorial.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class factorial 
+{
+    public static void main(String args[]) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your number? (below 20)");
+        System.out.println("(Only enter a number, no signs, no words, ONLY NUMBERS)");
+        System.out.println("Enter here:");
+        int num1 = scanner.nextInt();
+        int num2 = 1;
+
+        while (num1 > 1) 
+        {
+            num2 = num2 * num1;
+            num1--;
+        }
+
+        System.out.println("Factorial: " + num2);
+    }
+}
